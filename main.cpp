dce167b3108a4776b6b6d173406abe4105c30d63
Lorenzo <calderonlorenzo026@gmail.com>
2026-03-13 18:34:57 +0000

    created code for Quiz 3-2

--- main.cpp
+++ main.cpp
@@ -3,20 +3,24 @@
 #include <cmath>
 using namespace std;
 
+
 int main()
 {
     double a, b, c;
-    // TODO
+    
+    cin >> a >> b >> c;
 
-    // TODO
-    // TODO
-    // TODO
+    double discriminant = b * b - 4 * a * c;
 
-    // TODO
-    // TODO
+    double root1 = (-b + sqrt(discriminant)) / (2 * a);
+    double root2 = (-b - sqrt(discriminant)) / (2 * a);
 
     cout << setprecision(2) << fixed;
-    // TODO
 
+    if (root1 > root2)
+        cout << root1 << " " << root2;
+    else
+    cout << root2 << " " << root1;
+    
     return 0;
 }
